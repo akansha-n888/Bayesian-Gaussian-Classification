1eba992be0a33378ae619728e43391fa014b3a2d
akansha-n888 <74415318+akansha-n888@users.noreply.github.com>
2021-01-31 18:14:19 -0500

    Add files via upload

--- Part 1/runlab1.m
+++ Part 1/runlab1.m
@@ -56,3 +56,83 @@ trainingSet = [irisdata_features(1:100,:) numericLabels(1:100,1) ];
 
 
 %% Lab1 experiments (include here)
+
+x1 = [3.3 4.4 5 5.7 6.3];
+
+for i = 1:length(x1)
+   [posteriors_x,g_x]=lab1(x1(i),trainingSet,2);
+end
+
+
+f1=trainingSet(:,2);  % feature samples
+f2=trainingSet(:,1);  % feature samples
+la=trainingSet(:,5); % class labels
+
+m11 = mean(f1(find(la==1)))     % mean of the class conditional density p(x2/w1)
+std11 = std(f1(find(la==1)))    % Standard deviation of the class conditional density p(x2/w1)
+
+m12  = mean(f1(find(la==2)))    % mean of the class conditional density p(x2/w2)
+std12 = std(f1(find(la==2)))    % Standard deviation of the class conditional density p(x2/w2)
+
+syms th
+px = 0.7*(1/(sqrt(2*pi*std11)))*exp(-.5*((th-m11)/std11)^2)-1.3*(1/(sqrt(2*pi*std12)))*exp(-.5*((th-m12)/std12)^2); %since p(w1)=p(w2) it cancels on each eq
+Th1 = solve(px,th);
+Th1 = double(Th1);
+Th1 = (Th1(find(Th1>0)));
+
+errorTh1w2 = (length(f1(find(la==2&f1>Th1))))/length(f1(find(la==1|la==2)))
+
+
+
+disp(['The optimal Th for x1 is: ' num2str(Th1)])
+
+m11 = mean(f2(find(la==1)))     % mean of the class conditional density p(x2/w1)
+std11 = std(f2(find(la==1)))    % Standard deviation of the class conditional density p(x2/w1)
+
+m12  = mean(f2(find(la==2)))    % mean of the class conditional density p(x2/w2)
+std12 = std(f2(find(la==2)))    % Standard deviation of the class conditional density p(x2/w2)
+
+syms th
+px = (1/(sqrt(2*pi*std11)))*exp(-.5*((th-m11)/std11)^2)-(1/(sqrt(2*pi*std12)))*exp(-.5*((th-m12)/std12)^2); %since p(w1)=p(w2) it cancels on each eq
+Th2 = solve(px,th);
+Th2 = double(Th2);
+Th2 = (Th2(find(Th2>4)));
+
+disp(['The optimal Th for x2 is: ' num2str(Th2)])
+
+figure
+
+plot(irisdata_features(find(numericLabels(:)==1),1),irisdata_features(find(numericLabels(:)==1),2),'rs'); title('Th comparison for x_1 vs x_2');
+hold on;
+plot(irisdata_features(find(numericLabels(:)==2),1),irisdata_features(find(numericLabels(:)==2),2),'k.');
+xline(Th1)
+yline(Th2)
+xlabel('x_1')
+ylabel('x_2')
+axis([4 7 1 5]);
+
+errorX1 = (length(f1(find(la==1&f1<Th1)))+length(f1(find(la==2&f1>Th1))))/length(f1(find(la==1|la==2)))
+errorX2 = (length(f2(find(la==1&f2>Th2)))+length(f2(find(la==2&f2<Th2))))/length(f2(find(la==1|la==2)))
+
+%% Lab 1 Part 2
+
+x1 = [[2 6]; [4.4 3]; [5 3.5]; [5.3 2]; [5.5 2.5]; [6.6 3.5]; [4.5 6.1];]
+
+figure
+plot(irisdata_features(find(numericLabels(:)==1),1),irisdata_features(find(numericLabels(:)==1),2),'rs'); title('Th comparison for x_1 vs x_2');
+hold on;
+plot(irisdata_features(find(numericLabels(:)==2),1),irisdata_features(find(numericLabels(:)==2),2),'k.');
+hold on
+plot(x1(:,1),x1(:,2),'b*');
+xlabel('x_1')
+ylabel('x_2')
+axis([2 7.5 1 6.5]);
+
+for i = 1:length(x1)
+   [posteriors_x,a]=lab1(x1(i,1),trainingSet,1);
+   [posteriors_y,b]=lab1(x1(i,2),trainingSet,2);
+   disp(x1(i,:))
+   disp(['The g(x) = ' num2str(posteriors_x(1,1)+posteriors_y(1,1))])
+   disp(['The g(y) = ' num2str(posteriors_x(1,2)+posteriors_y(1,2))])
+   g_xy = a+b
+end
